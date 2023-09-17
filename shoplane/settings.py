fcf2e7000bc3367ee8712f0726a63b0d5982044e
Heena Syed <heena.afreen909@gmail.com>
2023-09-17 17:33:51 +0530

    changed allowed host

--- shoplane/settings.py
+++ shoplane/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f76=^jv7@d1cud=ai@$#6cx1v%f3yk76(2h6b@wv#@-udf7lrb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["13.236.209.225","localhost","127.0.0.1"]
 
 
 # Application definition
